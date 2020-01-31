7c44127f64b512b0d8b7280b78011026be133115
msultansyah <msultansyah11@gmail.com>
2020-01-31 18:29:06 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/sultan/latihan13/MainActivity.java
@@ -0,0 +1,48 @@
+package id.ac.poliban.mi.sultan.latihan13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText etLastName = findViewById(R.id.etLastName);
+        EditText etFirstName = findViewById(R.id.etFirstName);
+        Button btLoad = findViewById(R.id.btLoad);
+        Button btSave = findViewById(R.id.btSave);
+        TextView tvResult = findViewById(R.id.tvResult);
+
+        btSave.setOnClickListener(v -> {
+            SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor edit = sp.edit();
+
+            String lname,fname;
+            lname = etLastName.getText().toString();
+            fname = etFirstName.getText().toString();
+
+            edit.putString("lname", lname);
+            edit.putString("fname", fname);
+            edit.apply();
+
+            Toast.makeText(this, "data has been save it", Toast.LENGTH_SHORT).show();
+        });
+
+        btLoad.setOnClickListener(v -> {
+            SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+            String lname = sp.getString("lname", "na");
+            String fname = sp.getString("fname", "na");
+            tvResult.setText(String.format("%s %s", lname, fname));
+        });
+    }
+}
